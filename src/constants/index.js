3093105e269e9ce59dfd6ec7386fc81789ea410a
Keerthana <keerthanathirumoorthy20>
2023-10-16 21:41:30 +0530

    re

---
+++ src/constants/index.js
@@ -0,0 +1,60 @@
+const birds = [
+  {
+    id: 1,
+    name: "Perched blue and orange bird",
+    age: 8,
+    imgUrl:
+      "https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80",
+  },
+  {
+    id: 2,
+    name: "North American porcupine",
+    age: 5,
+    imgUrl:
+      "https://images.unsplash.com/photo-1622227056993-6e7f88420855?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    id: 3,
+    name: "Marshbird, brown and yellow",
+    age: 6,
+    imgUrl:
+      "https://images.unsplash.com/photo-1555169062-013468b47731?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    id: 4,
+    name: "Javan gold-spotted mongoose",
+    age: 4,
+    imgUrl:
+      "https://images.unsplash.com/photo-1628269165057-6bd6c5207760?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    id: 5,
+    name: "Rose-ringed parakeet",
+    age: 9,
+    imgUrl:
+      "https://images.unsplash.com/photo-1552728089-57bdde30beb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1450&q=80",
+  },
+  {
+    id: 6,
+    name: "Stilt, black-winged",
+    age: 4,
+    imgUrl:
+      "https://images.unsplash.com/photo-1597779610183-1a80c49a0436?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+  },
+  {
+    id: 7,
+    name: "Crab-eating fox",
+    age: 7,
+    imgUrl:
+      "https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80",
+  },
+  {
+    id: 8,
+    name: "Helmeted guinea fowl",
+    age: 6,
+    imgUrl:
+      "https://images.unsplash.com/photo-1563409236340-c174b51cbb81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1376&q=80",
+  },
+];
+
+export { birds };
